b3892b4593594f4d5e06d916fa9491cd8645b037
paul12lee1983 <78667258+paul12lee1983@users.noreply.github.com>
2021-09-25 12:01:57 +0100

    title changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const bg = document.querySelector(".hero")
+const title = document.getElementById('title')
+const description = document.getElementById('description')
 
 let load = 0;
 let int = setInterval(blurring, 15);
@@ -8,7 +10,8 @@ function blurring() {
   if (load > 99) {
     clearInterval(int);
   }
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  title.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  description.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
